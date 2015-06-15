10b9a47d0abe5a2039ec0f88cee93e42b341ebfe
noinstance <nunomp@gmail.com>
2015-06-15 02:26:51 +0100

    improved documentation

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
 # myex
 ## A MySql EXporter
+
 Exports a mysql database to a remote server.
+
 Replaces urls - useful for Wordpress.
 
 Requires mysqldump.
+
+### Options
+```
+  -d=value   destiny database name
+  -u=value   destiny user name
+  -p=value   destiny password
+  -D=value   origin database name, defaults to -d value
+  -f=value   backup filename, defaults to database name and timestamp
+  -w=dir     wordpress dir; reads from wp-config.php
+  -s=value   remote server 
+  -r=v1,v2   replaces v1 for v2 in the file; useful for absolute urls
+  -z         gzip output
+  -h         print the help screen
+```
+
+### Setup
+Make a copy of ```config.sample.php``` and rename to ```config.php```.
+Edit ```$origin``` and ```$destiny``` arrays to provide info of your local database server, remote servers and remote database servers.
+
+
+### Usage
+
+**Simple usage:** backup ``bloglocal``` database.
+
+```
+$ php myex.php -D=bloglocal
+```
+
+**Wordpress common scenario:** backup database (read name and user from ```wp-config.php``` in given directory), replace local urls for server urls, zip and restore on ```server1```.
+
+```
+$ php myex.php -w=/var/www/myblog -r=blog.local,blog.server1.com -z -s=server1
+```
+
+
+**All options:** backup local database ```bloglocal``` into file ```blog_backup.sql```, replace local urls for server urls, gzip it, send it to ```server1```, restore it there with the database name ```blogdev``` and user ```blog``` with password ```qwerty```.
+
+```
+$ php myex.php -D=bloglocal -f=blog_backup.sql -r=blog.local,blog.server1.com  -z -s=server1 -d=blogdev -u=blog -p=qwerty
+```
--- src/config.sample.php
+++ src/config.sample.php
@@ -1,43 +1,43 @@
 <?php
 
 $origin = [
-	'hostname' => 'localhost',
-	'port' => 3306,
-	'username' => 'root',
-	'password' => '1234'
+	'hostname' => 'localhost',				// local db server host
+	'port' => 3306,							// local db server port
+	'username' => 'root',					// local db server user with permission to run mysqldump
+	'password' => '1234'					// local db server user's password
 ];
 
 $destiny = [
-	'server1' => [
-		'ssh' => [
-			'hostname' => 'server1.com',
-			'port' => 22,
-			'username' => 'user',
-			'password' => 'pass',
-			'key' => false
+	'server1' => [							// you can have as many remote servers as you want
+		'ssh' => [							
+			'hostname' => 'server1.com',	// remote server ssh hostname (or ip)
+			'port' => 22,					// remote server ssh port
+			'username' => 'user',			// remote server login user
+			'password' => 'pass',			// remote server login password
+			// 'key' => false 				// not implemented
 		],
 		'db' => [
-			'hostname' => 'localhost',
-			'port' => 3306,
-			'username' => 'root',
-			'password' => '1234'
+			'hostname' => 'localhost',		// remote server database hostname
+			'port' => 3306,					// remote server database port
+			'username' => 'root',			// remote server user with permission to create users and databases
+			'password' => '1234'			// remote server user's password
 		]
 	],
-	'server2' => [
-		'ssh' => [
-			'hostname' => 'server2.com',
-			'port' => 22,
-			'username' => 'user',
-			'password' => 'pass',
-			'key' => false
-		],
-		'db' => [
-			'hostname' => 'localhost',
-			'port' => 3306,
-			'username' => 'root',
-			'password' => '1234'
-		]
-	]
+	// 'server2' => [						
+	// 	'ssh' => [
+	// 		'hostname' => 'server2.com',
+	// 		'port' => 22,
+	// 		'username' => 'user',
+	// 		'password' => 'pass',
+	// 		'key' => false
+	// 	],
+	// 	'db' => [
+	// 		'hostname' => 'localhost',
+	// 		'port' => 3306,
+	// 		'username' => 'root',
+	// 		'password' => '1234'
+	// 	]
+	// ]
 ];
 
 define('ORIGIN', serialize($origin));
