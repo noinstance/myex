5e0ca00e446dd8ee3e3e4a9ad5af2a609f603683
noinstance <nunomp@gmail.com>
2015-07-03 19:59:29 +0100

    =fixed serialized data with special charecters such as \n

--- src/lib/utils.php
+++ src/lib/utils.php
@@ -2,7 +2,19 @@
 class Utils {
 	public static function fixSerializedData ($data) {
 		return preg_replace_callback ( '!s:(\d+):"(.*?)";!', function($match) {
-	        	return ($match[1] == strlen($match[2])) ? $match[0] : 's:' . strlen($match[2]) . ':"' . $match[2] . '";';
+
+				$full_match = $match[0];
+				$len = $match[1];
+				$data = $match[2];
+
+				if($len == strlen($data)) {
+					// no biggie, all is cool
+					return $full_match;
+				} else {
+					// special characters like \n or \t should not be counted twice
+					$escape_count = substr_count($data, '\\');
+					return 's:' . (strlen($data) - $escape_count) . ':"' . $data . '";';
+				}
 		    },
 			str_replace('\"', '"', $data)
 		);
